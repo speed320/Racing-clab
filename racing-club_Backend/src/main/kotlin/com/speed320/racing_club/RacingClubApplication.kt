b09d654d9d975bca70a8ee50eae9ab1d6b4382db
Смирнов Сергей Александрович <sea.smirnov@bft.ru>
2025-06-05 19:29:19 +0300

    Init

---
+++ racing-club_Backend/src/main/kotlin/com/speed320/racing_club/RacingClubApplication.kt
@@ -0,0 +1,11 @@
+package com.speed320.racing_club
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RacingClubApplication
+
+fun main(args: Array<String>) {
+	runApplication<RacingClubApplication>(*args)
+}
---
+++ racing-club_Backend/src/test/kotlin/com/speed320/racing_club/RacingClubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.speed320.racing_club
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RacingClubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
